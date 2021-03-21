c1b0e780467339a3364b8497a8ca408bee3eddd1
ruanchaves <ruanchaves93@gmail.com>
2021-03-21 16:13:24 -0300

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -3,15 +3,15 @@ import os
 def get_preds(root_dir):
     output = []
     for root, dirs, files in os.walk(root_dir, topdown=False):
-    for name in dirs:
-        directory = os.path.join(root, name)
-        if 'submission_large' in directory:
-            preds = os.listdir(directory)
-            preds = [x for x in preds if x.startswith('preds')]
-            if preds:
-                preds = sorted(preds)[-1]
-                preds = os.path.join(directory, preds)
-                output.append(preds)
+        for name in dirs:
+            directory = os.path.join(root, name)
+            if 'submission_large' in directory:
+                preds = os.listdir(directory)
+                preds = [x for x in preds if x.startswith('preds')]
+                if preds:
+                    preds = sorted(preds)[-1]
+                    preds = os.path.join(directory, preds)
+                    output.append(preds)
     return output
 
 
