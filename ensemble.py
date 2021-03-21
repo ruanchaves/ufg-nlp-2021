79fed7a8d2341dc9706b3a747ab5a8d0f0dee891
ruanchaves <ruanchaves93@gmail.com>
2021-03-21 16:08:54 -0300

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -3,7 +3,7 @@ import os
 for root, dirs, files in os.walk(".", topdown=False):
    for name in dirs:
       directory = os.path.join(root, name)
-      if directory.startswith('submission_large'):
+      if 'submission_large' in directory:
           preds = os.listdir(directory)
           preds = [ x for x in preds if x.startswith('preds')]
           preds = sorted(preds)[-1]
