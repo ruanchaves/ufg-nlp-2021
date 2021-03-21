cfcf2ca450a4ff72ee5c6dd0a19bbcb9f471586d
ruanchaves <ruanchaves93@gmail.com>
2021-03-21 12:50:46 -0300

    final submission

--- run_glue.py
+++ run_glue.py
@@ -367,11 +367,28 @@ def main():
 
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
     # predictions and label_ids field) and has to return a dictionary string to float.
-    compute_metrics = make_compute_metrics(
-        model_args,
-        data_args,
-        training_args
-    )
+
+    def make_compute_metrics(training_args):
+        def compute_metrics(p: EvalPrediction):
+            preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+
+            filename = 'preds_{0}.npy'.format(int(time.time()))
+            path = os.path.join(training_args.output_dir, filename)
+            np.save(path, preds)
+
+            preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+            if data_args.task_name is not None:
+                result = metric.compute(predictions=preds, references=p.label_ids)
+                if len(result) > 1:
+                    result["combined_score"] = np.mean(list(result.values())).item()
+                return result
+            elif is_regression:
+                return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
+            else:
+                return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
+        return compute_metrics
+
+    compute_metrics = make_compute_metrics(training_args)
 
     # Data collator will default to DataCollatorWithPadding, so we change it if we already did the padding.
     if data_args.pad_to_max_length:
---
+++ test.sh
@@ -0,0 +1,15 @@
+python run_glue.py \
+  --model_name_or_path neuralmind/bert-base-portuguese-cased \
+  --do_train \
+  --do_eval \
+  --evaluation_strategy steps \
+  --eval_steps 500 \
+  --train_file train_total.csv \
+  --validation_file test_final.csv \
+  --max_seq_length 256 \
+  --per_device_train_batch_size 4 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 5e-05 \
+  --num_train_epochs 7.5 \
+  --weight_decay 0.01 \
+  --output_dir ./submission
