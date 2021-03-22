3b32ab6ab3ab53cf014f561ac57839193872b8a2
ruanchaves <ruanchaves93@gmail.com>
2021-03-22 05:34:53 -0300

    seeds

---
+++ best_seed.sh
@@ -0,0 +1,16 @@
+python best_seed.py \
+  --model_name_or_path neuralmind/bert-large-portuguese-cased \
+  --do_eval \
+  --evaluation_strategy steps \
+  --eval_steps 500 \
+  --save_strategy no \
+  --train_file train_2.csv \
+  --validation_file dev_2.csv \
+  --max_seq_length 256 \
+  --per_device_train_batch_size 4 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 5e-05 \
+  --num_train_epochs 7.5 \
+  --weight_decay 0.01 \
+  --output_dir /home/params_seed \
+  --time_budget_s $TIME_BUDGET
